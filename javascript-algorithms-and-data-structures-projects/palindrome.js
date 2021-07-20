6f60ed17ee05b656ce91db9b444c366be159e839
Sizhe Cao <cao20@up.edu>
2021-07-20 15:53:53 -0700

    JavaScript Palindrom exercise

---
+++ javascript-algorithms-and-data-structures-projects/palindrome.js
@@ -0,0 +1,13 @@
+function palindrome(str) {
+    //sanitize the string into char array
+    let arr = str.toLowerCase().replace(/\W|_/g, "").split("");
+    while(arr.length > 1){
+      if (arr[0] !== arr[arr.length-1]) return false;
+      arr.pop();
+      arr.shift();
+    }
+    return true;
+}
+
+console.log(palindrome("A man, a plan, a canal. Panama"));
+console.log(palindrome("0_0 (: /-\ :) 0-0"));
